3e04955bb7285af7c0e446b3262a5fa730b57522
LuisFe Gomez <50106079+luisfegomezPE@users.noreply.github.com>
2020-12-12 17:33:34 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-basic.forever(function on_forever() {
-    for (let index = 0; index < 5; index++) {
+basic.forever(function () {
+    for (let index = 0; index <= 4; index++) {
         led.plot(index, index)
         basic.pause(100)
     }
-    //  repeat for loop from here
-    for (let index2 = 0; index2 < 5; index2++) {
+    // repeat for loop from here
+    for (let index2 = 0; index2 <= 4; index2++) {
         led.unplot(index2, index2)
         basic.pause(100)
     }
